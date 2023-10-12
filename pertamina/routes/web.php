1fa8e21072c339e7484dd8b3206279fb50371de3
faizarindra <faozer13@gmail.com>
2023-10-13 00:15:11 +0700

    login

--- pertamina/routes/web.php
+++ pertamina/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('welcome');
 });
+Route::get('/admin-dashboard', [AdminController::class, 'dashboard']);
+
